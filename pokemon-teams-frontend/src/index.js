c8650f690988ecbe4182cbd66d7ab4a63e643508
Safae <safae.alaoui@flatironschool.com>
2019-07-22 11:15:17 -0400

    solution

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -1,3 +1,44 @@
 const BASE_URL = "http://localhost:3000"
 const TRAINERS_URL = `${BASE_URL}/trainers`
 const POKEMONS_URL = `${BASE_URL}/pokemons`
+
+document.addEventListener('DOMContentLoaded', setUpPage)
+
+function setUpPage(){
+
+    fetchTrainers()
+
+}
+
+function fetchTrainers(){
+    fetch(TRAINERS_URL)
+    .then(response => response.json())
+    .then(addAllTrainers)
+}
+
+let addAllTrainers = (trainers)=> {
+    trainers.forEach(addTrainerCard)
+
+    //query select all buttons
+    //add event listener to buttons
+
+}
+
+
+function addTrainerCard(trainer){
+    let pokemons = trainer.pokemons.map(pokemon=>{
+        let li = `<li>${pokemon.nickname} (${pokemon.species}) <button class="release" data-pokemon-id=${pokemon.id}>Release</button></li>`
+        return li 
+    })
+
+    let trainerHTML = `<div class="card" data-id=${trainer.id}><p>${trainer.name}</p>
+    <button data-trainer-id=${trainer.id}>Add Pokemon</button>
+    <ul>`
+      + pokemons.join(" ") + 
+  `</ul>
+  </div>`
+
+    
+}
+
+
